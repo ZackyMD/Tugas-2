8b23fc27f01bc467cb26958ad701d4514a6fbab4
Your Namegit <you@example.com>
2022-09-14 10:14:32 +0700

    Bismillah Final

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # TODO: Create your views here.
+from django.shortcuts import render
+from django.shortcuts import render
+from katalog.models import CatalogItem
+
+# Create your views here.
+
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Zacky Muchlas Dharmawan'
+        
+    }
+    return render(request, "katalog.html", context)
